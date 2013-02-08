065fc11950e69a4974ec333d128a6225c2efca3d
Charles Miglietti <charles.miglietti@yahoo.fr>
2013-02-08 14:13:07 +0100

    separate concerns

--- server.js
+++ server.js
@@ -1,80 +1,49 @@
 #! /usr/local/bin/node
 
-var nodemailer = require('nodemailer')
-  , h4e = require('h4e')
-  , exec = require('child_process').exec
-  , redis = require('redis')
+var exec = require('child_process').exec
   , _ = require('underscore')
-  , client = redis.createClient()
-  , fs = require("fs")
-  , jquery = fs.readFileSync("./jquery.js").toString()
-  , smtpTransport
-  , matches;
+  , mailer = require('./lib/mailer')
+  , storage = require('./lib/storage.js')
+  , patterns = ['jquery', 'bootstrap'];
 
 
-smtpTransport = nodemailer.createTransport('SMTP',{
-    host : 'smtp.mandrillapp.com',
-    port : '587',
-    auth: { user: process.env.MAILER_USERNAME
-          , pass: process.env.MAILER_PWD
-          }
-});
-
-h4e.setup({ extension: 'mustache'
-          , baseDir: './emails'
-          , toCompile: ['*'] });
-
-
-function sendEmail (options, cb) {
-  var to = 'hello+test@tldr.io'
-    , from = 'tldr.io <hello@tldr.io>'
-    , type = options.type
-    , values = options.values || {}
-    , mailOptions
-    , cb = cb || function () {};
-
-  mailOptions = { from: from
-                , to: to
-                , subject: 'Test'
-                , html: h4e.render(type, { values: values })
-                };
-
-  // Send mail with defined transport object. Log only if there is an error
-  smtpTransport.sendMail(mailOptions, function(error, response) {
-    if(error) {
-      console.log('Error sending email with type' + type, error);
-      return;
-    }
-    console.log('Mail sent correctly');
-    cb();
-  });
-};
-
-
-
-exec('phantomjs HNFrontpageScraper.js', function (err, stdout, stderr) {
+exec('phantomjs lib/HNFrontpageScraper.js ' + patterns.join(' '), function (err, stdout, stderr) {
 
   var res = JSON.parse(stdout)
     , options
-    , valuesToSend = []
-    , toSend
+    , valuesToSend = [] // data needed for the email templating
+    , toSend // array of the items which matches the pattern but which havent been sent yet
     , matches = res.matches;
 
-  client.hkeys('scraper matches', function (err, replies) {
-    toSend = _.difference(_.pluck(matches, 'title'), replies);
-    toSend.forEach( function (element, i) {
-      client.hset('scraper matches', element, JSON.stringify(matches[i]), redis.print);
-      valuesToSend.push(matches[i]);
-    });
-
-    if (valuesToSend.length) {
-      options = { values: { matches: valuesToSend }
-                , type: 'alertScraper' }
-
-      sendEmail(options, function () {process.exit(0);})
+  storage.getKeys(function (err, replies) {
+    if (err) {
+      console.error('Error while getting the keys');
     } else {
-      console.log('Nothing to be noticed');
-      process.exit(0);
+      // Compute the difference between the matches that triggered the alert and
+      // and the ones that have already been sent
+      toSend = _.difference(_.pluck(matches, 'title'), replies);
+      toSend.forEach( function (element, i) {
+        valuesToSend.push(matches[i]);
+      });
+
+      // Check if there is something to send
+      if (toSend.length) {
+        options = { values: { matches: valuesToSend
+                            , patterns: patterns }
+                  , type: 'alertScraper' };
+
+        // Send mail
+        mailer.sendMail(options, function () {
+          // Update the items that have been sent
+          toSend.forEach( function (element, i) {
+            storage.setKV(element, matches[i]);
+          });
+          process.exit(0);
+        })
+      } else {
+        console.log('Nothing to be noticed');
+        process.exit(0);
+      }
     }
   });
 });
