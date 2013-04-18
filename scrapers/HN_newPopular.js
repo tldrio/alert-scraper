fbc51b8f0eaf01ce361e49cae894733d68aea54e
Louis Chatriot <louis.chatriot@gmail.com>
2013-04-18 18:44:58 +0200

    Able to get all links on the HN frontpage

---
+++ scrapers/HN_newPopular.js
@@ -0,0 +1,21 @@
+var request = require('request')
+  , cheerio = require('cheerio')
+  ;
+
+request.get({ url: 'https://news.ycombinator.com/' }, function (err, res, body) {
+  var $ = cheerio.load(body)
+    , $links = $('a')
+    , i
+    ;
+
+  console.log('---------------');
+  console.log('---------------');
+  console.log('---------------');
+
+  for (i = 0; i < $links.length; i += 1) {
+    console.log($($links[i]).attr('href'));
+    console.log("===============");
+  }
+
+
+})
