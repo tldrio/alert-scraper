27ad71bb9c179258428e62662acef599e3c259ab
Louis Chatriot <louis.chatriot@gmail.com>
2013-04-19 09:21:39 +0200

    Able to scrape only actual HN links with the number of comments

--- scrapers/HN_newPopular.js
+++ scrapers/HN_newPopular.js
@@ -2,20 +2,42 @@ var request = require('request')
   , cheerio = require('cheerio')
   ;
 
+/**
+ * From a "xxx comments/discuss" string, get the number of comments
+ */
+function getNumberOfComment (s) {
+  var res = parseInt(s, 10);
+
+  if (isNaN(res)) {
+    return 0;
+  } else {
+    return res;
+}
+  }
+
+
 request.get({ url: 'https://news.ycombinator.com/' }, function (err, res, body) {
   var $ = cheerio.load(body)
     , $links = $('td.title a')
+    , $link, $comment
+    , items = []
     , i
     ;
 
-  console.log('---------------');
-  console.log('---------------');
-  console.log('---------------');
+  for (i = 0; i < 30; i += 1) {
+    $link = $($links[i]);
+    $comment = $link.parent().parent().next().find('a:nth-child(3)');
 
-  for (i = 0; i < $links.length; i += 1) {
-    console.log($($links[i]).attr('href'));
-    console.log("===============");
+    if ($comment.html()) {
+      items.push({ number: i
+                 , link: $link.attr('href')
+                 , comments: getNumberOfComment($comment.html())
+                 });
+    }
   }
+
+
+  console.log(items);
 
 
 })
